cae48470705551e7208adda5b5642ebd87ac14c3
Rajat Singh <junkee@Rajats-MacBook-Pro.local>
2021-03-06 04:38:17 -0500

    Added Various Component for the Project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
+import { RouterModule } from '@angular/router';
 import { environment } from './../environments/environment';
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -10,10 +11,31 @@ import { AngularFireModule } from '@angular/fire';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { AngularFireStorageModule } from '@angular/fire/storage';
 import { AngularFireAuthModule } from '@angular/fire/auth';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { ProductsComponent } from './products/products.component';
+import { ShopingCartComponent } from './shoping-cart/shoping-cart.component';
+import { CheckoutComponent } from './checkout/checkout.component';
+import { OrderSuccessComponent } from './order-success/order-success.component';
+import { MyOrderComponent } from './my-order/my-order.component';
+import { AdminProductsComponent } from './admin/admin-products/admin-products.component';
+import { AdminOrdersComponent } from './admin/admin-orders/admin-orders.component';
+import { LoginComponent } from './login/login.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    ProductsComponent,
+    ShopingCartComponent,
+    CheckoutComponent,
+    OrderSuccessComponent,
+    MyOrderComponent,
+    AdminProductsComponent,
+    AdminOrdersComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
@@ -21,7 +43,19 @@ import { AngularFireAuthModule } from '@angular/fire/auth';
     AngularFireModule.initializeApp(environment.firebase),
     AngularFirestoreModule,
     AngularFireAuthModule,
-    AngularFireStorageModule
+    AngularFireStorageModule,
+    NgbModule,
+    RouterModule.forRoot([
+      {path: '', component: HomeComponent},
+      {path: 'products', component: ProductsComponent},
+      {path: 'shopping-cart', component: ShopingCartComponent},
+      {path: 'check-out', component: CheckoutComponent},
+      {path: 'order-success', component: CheckoutComponent},
+      {path: 'login', component: LoginComponent},
+      {path: 'admin/admin-products', component: AdminProductsComponent},
+      {path: 'admin/admin-orders', component: AdminOrdersComponent},
+      {path: 'my-orders', component: MyOrderComponent}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
